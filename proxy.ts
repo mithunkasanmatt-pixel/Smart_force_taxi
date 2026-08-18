3d57f04c2020cf087f9bf8497aaaa06f57561445
Mithunkumar <mithunkasanmatt@gmail.com>
2026-08-18 20:47:45 +0530

    add test-auth diagnostics endpoint

--- proxy.ts
+++ proxy.ts
@@ -73,5 +73,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/((?!api/debug|_next/static|_next/image|favicon.ico|public|logo\\.png|hero\\.mp4|images|.*\\..*$).*)"],
+  matcher: ["/((?!api/debug|api/test-auth|_next/static|_next/image|favicon.ico|public|logo\\.png|hero\\.mp4|images|.*\\..*$).*)"],
 };
